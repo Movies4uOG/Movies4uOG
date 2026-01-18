b5ec82312015400db2a00b0b6e8134ffefd19a9c
Movies4uOG <prathmeshx3303@gmail.com>
2026-01-18 22:59:40 +0530

    Update customData.js

--- customData.js
+++ customData.js
@@ -5,3 +5,12 @@ const customData = [
     link1080p: "https://linkpays.in/k1IZCI"
   }
 ];
+,
+{
+  id: 1291608,          // TMDB ID of new movie
+  type: "movie",
+  links: {
+    "1080p – Server 1": "https://linkpays.in/GnZqA9E",
+    "1080p – Server 2": "https://linkpays.in/GnZqA9E"
+  }
+}
