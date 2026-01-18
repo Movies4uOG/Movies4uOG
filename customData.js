e28e020bd2197c380d089a218fa3483d9a73590f
Movies4uOG <prathmeshx3303@gmail.com>
2026-01-18 22:18:43 +0530

    Update customData.js

--- customData.js
+++ customData.js
@@ -1,14 +1,7 @@
 const customData = [
   {
-    id: 575588,  // TMDB movie id (example: KGF Chapter 2)
+    id: 1038392,          // TMDB movie ID
     type: "movie",
-    streaming: "https://www.netflix.com/title/12345",
-    download: "https://example.com/kgf2-download"
-  },
-  {
-    id: 1402,  // TMDB tv series id (example)
-    type: "tv",
-    streaming: "https://www.primevideo.com/detail/0T1",
-    download: "https://example.com/series-download"
+    link1080p: "https://linkpays.in/k1IZCI"
   }
 ];
